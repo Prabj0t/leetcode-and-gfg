dd88372e7005bf7e0a9be0a0b6d3be541968103b
Prabj0t <117665616+Prabj0t@users.noreply.github.com>
2022-11-13 13:24:49 +0530

    Added solution - LeetHub

--- Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.cpp
+++ Check if Linked List is Palindrome - GFG/check-if-linked-list-is-palindrome.cpp
@@ -29,38 +29,107 @@ struct Node {
 };
 */
 
-class Solution{
-  public:
+// class Solution{
+  
     //Function to check whether the list is palindrome.
-    void insertathead(Node *&head, int key)
-{
-    // new node created
-    Node *temp = new Node(key);
-    temp->next = head;
-    head = temp;
-}
+//     void insertathead(Node *&head, int key)
+// {
+//     // new node created
+//     Node *temp = new Node(key);
+//     temp->next = head;
+//     head = temp;
+// }
 
+    // bool isPalindrome(Node *head)
+    // {
+    //     //Your code here
+    //     Node * curr = head;
+    //     Node * H2 = new Node(curr->data);
+    //     while(curr->next != NULL){
+    //         curr = curr -> next;
+    //         insertathead(H2 ,curr->data);
+    //     }
+    //     Node * t1 = head;
+    //     Node * t2 = H2;
+    //     while( t1 != NULL && t2 != NULL){
+    //         if ( t1 -> data == t2 -> data ){
+    //             t1 = t1 -> next ;
+    //             t2 = t2 -> next ;
+    //         }    
+    //         else{
+    //             return 0 ;
+    //         }
+    //     }
+    //     return 1;
+    // }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+class Solution{    
+   
+   public:
+    Node * getmid(Node * head)
+    {
+        Node * slow = head ;
+        Node * fast = head ;
+        while(fast-> next  != NULL && fast ->  next -> next != NULL){
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+        return slow;
+    }
+    
+    Node* reverse(Node * head)
+    {
+        Node* prev = NULL;
+        Node* cur = head;
+        Node * fwd = NULL ;                                      
+        while ( cur != NULL)
+        {
+            fwd = cur -> next ;
+            cur -> next = prev ;
+            prev = cur ;
+            cur = fwd ;
+        
+        }
+    
+    return prev;
+    
+    }
+    
+    
     bool isPalindrome(Node *head)
     {
         //Your code here
-        Node * curr = head;
-        Node * H2 = new Node(curr->data);
-        while(curr->next != NULL){
-            curr = curr -> next;
-            insertathead(H2 ,curr->data);
+        if (head -> next == NULL){
+            return true;
         }
-        Node * t1 = head;
-        Node * t2 = H2;
-        while( t1 != NULL && t2 != NULL){
-            if ( t1 -> data == t2 -> data ){
-                t1 = t1 -> next ;
-                t2 = t2 -> next ;
-            }    
-            else{
-                return 0 ;
+        
+        Node * mid = getmid(head);
+        Node * t = mid -> next;
+        mid->next = reverse(t);
+        
+        Node * temp = head;
+        Node * temp2 = mid -> next;
+        while ( temp2 != NULL ){
+        
+            if( temp-> data != temp2 -> data)
+            {
+                return false;
             }
+            temp = temp-> next ;
+            temp2 = temp2 -> next;
         }
-        return 1;
+        return true;
     }
 };
 
