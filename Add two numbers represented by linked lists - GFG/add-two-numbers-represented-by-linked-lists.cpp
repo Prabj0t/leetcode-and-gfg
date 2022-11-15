442c8d509c60941636d44650f714ed7b974e3e37
Prabj0t <117665616+Prabj0t@users.noreply.github.com>
2022-11-15 15:10:32 +0530

    Added solution - LeetHub

--- Add two numbers represented by linked lists - GFG/add-two-numbers-represented-by-linked-lists.cpp
+++ Add two numbers represented by linked lists - GFG/add-two-numbers-represented-by-linked-lists.cpp
@@ -57,6 +57,106 @@ struct Node {
 
 */
 
+// class Solution
+// {
+//     public:
+//     //Function to add two numbers represented by linked list.
+    
+//      Node* reverse(Node * head)
+//     {
+//         Node* prev = NULL;
+//         Node* cur = head;
+//         Node * fwd = NULL ;                                      
+//         while ( cur != NULL)
+//         {
+//             fwd = cur -> next ;
+//             cur -> next = prev ;
+//             prev = cur ;
+//             cur = fwd ;
+        
+//         }
+    
+//     return prev;
+    
+//     }
+//     void insertattail( struct Node * &head , struct Node* &tail , int data){
+//         Node *temp = new Node(data);
+//         if ( head == NULL )
+//         {
+//             head = temp;
+//             tail = temp;
+//             return;
+//         }
+//         else {
+//             tail-> next = temp;
+//             tail = temp;
+//         }
+//     }
+    
+//     struct Node* addition( struct Node* first , struct Node* second){
+//         int carry = 0;
+//         int digit = 0;
+//         int sum = 0;
+//         Node * f1 = first ;
+//         Node * f2 = second;
+        
+//         Node * anshead = NULL ;
+//         Node * anstail = NULL ;
+//         while (f1 != NULL &&  f2 != NULL ){
+//             sum = carry + (f1->data) + (f2->data);
+//             digit = sum % 10;
+//             carry = sum / 10;
+            
+//             insertattail(anshead , anstail , digit );
+//             f1= f1-> next;
+//             f2 = f2-> next;
+//         }
+//         while ( f1 != NULL ){
+//             sum = carry + f1->data;
+//             digit = sum % 10;
+//             carry = sum / 10;
+//             insertattail(anshead , anstail , digit );
+//             f1= f1-> next;
+//         }
+        
+//         while ( f2 != NULL ){
+//             sum = carry + f2->data;
+//             digit = sum % 10;
+//             carry = sum / 10;
+//             insertattail(anshead , anstail , digit );
+//             f2= f2-> next;
+//         }
+//         while ( carry != 0){
+//             sum = carry;
+//             digit = sum % 10;
+//             insertattail(anshead , anstail , digit );
+//             carry = sum / 10;
+//         }
+//         return anshead;
+//     }
+//     struct Node* addTwoLists(struct Node* first , struct Node* second)
+//     {
+//         // code here
+//         first  = reverse(first);
+//         second = reverse(second);
+       
+//         Node * ans = addition( first , second );
+        
+//         ans = reverse( ans );
+        
+//         return ans;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
 class Solution
 {
     public:
@@ -79,7 +179,9 @@ class Solution
     return prev;
     
     }
-    void insertattail( struct Node * &head , struct Node* &tail , int data){
+    
+    void insertattail( struct Node * &head , struct Node* &tail , int data)
+    {
         Node *temp = new Node(data);
         if ( head == NULL )
         {
@@ -93,47 +195,43 @@ class Solution
         }
     }
     
-    struct Node* addition( struct Node* first , struct Node* second){
+    struct Node* addition( struct Node* f1 , struct Node* f2)
+    {
         int carry = 0;
         int digit = 0;
         int sum = 0;
-        Node * f1 = first ;
-        Node * f2 = second;
         
         Node * anshead = NULL ;
         Node * anstail = NULL ;
-        while (f1 != NULL &&  f2 != NULL ){
-            sum = carry + (f1->data) + (f2->data);
-            digit = sum % 10;
-            carry = sum / 10;
-            
-            insertattail(anshead , anstail , digit );
-            f1= f1-> next;
-            f2 = f2-> next;
+        
+        while (f1 != NULL ||  f2 != NULL || carry != 0){
+        int val1 = 0 ;
+        if( f1 != NULL ){
+        val1 = f1-> data; 
         }
-        while ( f1 != NULL ){
-            sum = carry + f1->data;
-            digit = sum % 10;
-            carry = sum / 10;
-            insertattail(anshead , anstail , digit );
+            
+        int val2 = 0 ;
+        if( f2 != NULL ){
+        val2 = f2 -> data ;
+        }
+            
+        sum = carry + val1 + val2;
+        digit = sum % 10;
+        insertattail(anshead , anstail , digit );
+        carry = sum / 10;
+            
+        if ( f1 != NULL ){
             f1= f1-> next;
         }
         
-        while ( f2 != NULL ){
-            sum = carry + f2->data;
-            digit = sum % 10;
-            carry = sum / 10;
-            insertattail(anshead , anstail , digit );
-            f2= f2-> next;
+        if ( f2 != NULL ){
+            f2= f2-> next;     
+            
         }
-        while ( carry != 0){
-            sum = carry;
-            digit = sum % 10;
-            insertattail(anshead , anstail , digit );
-            carry = sum / 10;
-        }
-        return anshead;
     }
+      
+    return anshead;
+}
     struct Node* addTwoLists(struct Node* first , struct Node* second)
     {
         // code here
