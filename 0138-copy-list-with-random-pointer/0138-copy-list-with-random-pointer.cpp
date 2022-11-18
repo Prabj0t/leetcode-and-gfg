58c3588fce84cdeb0562517fb7ef0c3c92bbaefa
Prabj0t <117665616+Prabj0t@users.noreply.github.com>
2022-11-18 13:01:53 +0530

    Time: 4 ms (98.10%), Space: 11.2 MB (59.95%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -13,46 +13,98 @@ public:
     }
 };
 */
-class Solution {
+//  SOLUTION WITH T.C = O(N)  AND   S.C = (1)
+
+
+class Solution
+{
 private:
- void insertattail( Node * &head ,Node* &tail , int data){
-     Node * newNode = new Node(data);
-     if( head == NULL ){
-         head = newNode;
-         tail = newNode;
-         return;
-     }
-     else{
-     tail-> next = newNode;
-     tail = newNode;}
- }
+    void insertattail(Node *&head, Node *&tail, int data)
+    {
+        Node *newNode = new Node(data);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
 public:
-    Node* copyRandomList(Node* head) {
-        
-        Node * clonehead = NULL ;
-        Node * clonetail = NULL ;
-        
-        Node * original = head;
-        while ( original != NULL ){
-            insertattail( clonehead , clonetail , original-> val);
-            original = original -> next;
+    Node *copyRandomList(Node *head)
+    {
+
+        Node *clonehead = NULL;
+        Node *clonetail = NULL;
+
+        Node *original = head;
+        while (original != NULL)
+        {
+            insertattail(clonehead, clonetail, original->val);
+            original = original->next;
         }
-        unordered_map< Node* , Node*> oldtonew ;
-        original = head ;
-        Node * temp2 = clonehead;
-        while ( original != NULL && temp2 != NULL ){
-            oldtonew[original] = temp2;
-            original = original -> next;
-            temp2 = temp2 -> next;
+        // unordered_map< Node* , Node*> oldtonew ;
+        original = head;
+        Node *temp2 = clonehead;
+        Node *fwd = original;
+        Node *fwd2 = temp2;
+        while (original != NULL && temp2 != NULL)
+        {
+            // oldtonew[original] = temp2;
+            fwd = original->next;
+            original->next = temp2;
+            original = fwd;
+
+            fwd2 = temp2->next;
+            temp2->next = original;
+            temp2 = fwd2;
+
+            // original = original -> next;
+            // temp2 = temp2 -> next;
         }
-        original = head ;
+        original = head;
         temp2 = clonehead;
-        while (original != NULL && temp2 != NULL){
-            temp2 -> random = oldtonew[original -> random];
-            temp2 = temp2 -> next;
-            original  = original -> next ;
+        while (original != NULL && temp2 != NULL)
+        {
+
+            if (original->next != NULL)
+            {
+                if (original->random != NULL)
+                {
+                    temp2->random = original->random->next;
+                }
+                else
+                {
+                    temp2->random = NULL;
+                }
+            }
+            if (original->next != NULL)
+            {
+                original = original->next->next;
+            }
+            if (original != NULL)
+            {
+                temp2 = original->next;
+            }
+        }
+        original = head;
+        temp2 = clonehead;
+        while (temp2 != NULL && original != NULL)
+        {
+            original->next = original->next->next;
+            original = original->next;
+
+            if (original != NULL)
+            {
+                temp2->next = original->next;
+            }
+            temp2 = temp2->next;
         }
         return clonehead;
-        
     }
 };
