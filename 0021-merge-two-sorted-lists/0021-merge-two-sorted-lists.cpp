0a56e432516e055ea1f71d1d98aeaed3f2755a5c
Prabj0t <117665616+Prabj0t@users.noreply.github.com>
2023-06-06 01:10:21 +0530

    Time: 12 ms (33.77%), Space: 14.7 MB (67.45%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,29 +11,53 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* temp = new ListNode();
-        ListNode* a = list1;
-        ListNode* res= temp;
-        ListNode* b = list2;
-        while(a != NULL && b != NULL){
-            if( a->val <= b->val){
-                res->next = a;
-                res = res->next;
-                a = a-> next;
-            }
-            else{
-                res->next = b;
-                res = res->next;
-                b = b->next;
-                }
+//         recursive approach
+        ListNode* res = NULL;
+        if(list1 == NULL){
+           
+            return list2;
         }
-        if(a == NULL){
-            res->next = b;
+        if(list2 == NULL){
+            return list1;
         }
-        if(b == NULL){
-            res->next = a;
+        
+        if(list1->val <= list2->val){
+            res = list1;
+            res->next = mergeTwoLists(list1->next , list2);
         }
-        res = temp->next;
+        
+        else{
+            res = list2;
+            res->next = mergeTwoLists(list1 , list2->next);
+        }
         return res;
     }
 };
+
+
+// ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         ListNode* temp = new ListNode();
+//         ListNode* a = list1;
+//         ListNode* res= temp;
+//         ListNode* b = list2;
+//         while(a != NULL && b != NULL){
+//             if( a->val <= b->val){
+//                 res->next = a;
+//                 res = res->next;
+//                 a = a-> next;
+//             }
+//             else{
+//                 res->next = b;
+//                 res = res->next;
+//                 b = b->next;
+//                 }
+//         }
+//         if(a == NULL){
+//             res->next = b;
+//         }
+//         if(b == NULL){
+//             res->next = a;
+//         }
+//         res = temp->next;
+//         return res;
+//     }
