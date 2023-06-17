b825ecfb8a0fd1a7e01cdffe262c8563fa3a4eab
Prabj0t <117665616+Prabj0t@users.noreply.github.com>
2023-06-17 23:48:55 +0530

    Time: 11 ms (34.17%), Space: 11.7 MB (24.24%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -0,0 +1,108 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) 
+    {
+        if( head == NULL || head-> next == NULL || k == 0){
+            return head;
+        }
+        
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+        int size = 1;
+        while(temp->next != NULL){
+            temp = temp -> next;
+            size++;
+        }
+        
+        if(size < k)
+        {
+            k = k %  size;
+        }
+        int i = 1;
+        while(i <= k)
+        {
+            ListNode* temp = head;
+            ListNode* prev = NULL;
+
+            while(temp->next != NULL){
+                prev = temp; 
+                temp = temp -> next;
+            }
+
+                temp->next = head;
+                prev->next = NULL;
+                head = temp;
+                prev = NULL;
+                i++;
+            }
+        return head;
+    }
+        
+};
+
+
+
+
+
+
+
+
+
+
+// if( head == NULL || head-> next == NULL){
+//             return head;
+//         }
+        
+//         ListNode* temp = head;
+//         ListNode* prev = NULL;
+//         int size = 1;
+//         while(temp->next != NULL){
+//             temp = temp -> next;
+//             size++;
+//         }
+//         int i = 1;
+//         if(size < k)
+//         {
+//             int i = 1;
+//             while(i <= k)
+//             {
+//                 ListNode* temp = head;
+//                 ListNode* prev = NULL;
+
+//                 while(temp->next != NULL){
+//                     prev = temp; 
+//                     temp = temp -> next;
+//             }
+
+//                 temp->next = head;
+//                 prev->next = NULL;
+//                 head = temp;
+//                 prev = NULL;
+//                 i++;
+//             }
+//         }
+//         else
+//         {
+//             temp->next = head;
+//             ListNode* t = head;
+
+//             int i = 0;
+//             while( i < size-k-1 ){
+//                 t = t-> next;
+//                 i++;
+//             }
+//             head = t -> next;
+//             t->next = NULL;
+//         }
+//         return head;
+    
