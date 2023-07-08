0ccf91068b6ce064e3fcb14068683ceb7f32b204
Prabj0t <117665616+Prabj0t@users.noreply.github.com>
2023-07-09 01:16:21 +0530

    Time: 5 ms (72.15%), Space: 8.8 MB (55.85%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,28 +1,54 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> ans;
-        stack<int> s;
+//         vector<int> ans;
         
-        for( int i = 0 ; i < nums1.size() ; i++){
-            int j;
-            for(  j = 0 ; j < nums2.size() ; j++){
-                if(nums1[i] == nums2[j]){
-                    break;
-                }
+//         for( int i = 0 ; i < nums1.size() ; i++){
+//             int j;
+//             for(  j = 0 ; j < nums2.size() ; j++){
+//                 if(nums1[i] == nums2[j]){
+//                     break;
+//                 }
+//             }
+//             int k =0;
+//             for(k = j+1 ; k < nums2.size() ; k++){
+//                 if(nums2[j] < nums2[k]){
+//                     ans.push_back(nums2[k]);
+//                     break;
+//                 }
+//             }
+//             if( k == nums2.size()){
+//                 ans.push_back(-1);
+//             }
+//         }
+        
+//         return ans;
+//     }
+        
+//         with stack and unordered map
+        
+        vector<int> ans (nums1.size(), -1);
+        stack<int> s;
+        unordered_map<int,int> umap;
+        
+        for(int i = 0 ; i < nums2.size() ; i++){
+            
+            while( !s.empty() && s.top() < nums2[i]){
+                
+                umap[s.top()] = nums2[i];
+                s.pop();
             }
-            int k =0;
-            for(k = j+1 ; k < nums2.size() ; k++){
-                if(nums2[j] < nums2[k]){
-                    ans.push_back(nums2[k]);
-                    break;
-                }
-            }
-            if( k == nums2.size()){
-                ans.push_back(-1);
-            }
+            
+            s.push(nums2[i]);
         }
         
+        for( int j = 0 ; j < nums1.size() ; j++){
+            
+            if(umap.find(nums1[j]) != umap.end()){
+                ans[j] = umap[nums1[j]];
+            }
+            
+        }
         return ans;
     }
 };
