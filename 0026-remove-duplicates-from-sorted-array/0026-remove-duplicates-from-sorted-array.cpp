b550d2ffaeb669d5da836e0d7416909c69e44916
Prabj0t <117665616+Prabj0t@users.noreply.github.com>
2023-07-09 00:38:23 +0530

    Time: 7 ms (94.55%), Space: 18.3 MB (60.01%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,36 +1,55 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
+    // int removeDuplicates(vector<int>& nums) {
         
-        int i = 0 ;
-        int j = nums.size()-1;
+//         int i = 0 ;
+//         int j = nums.size()-1;
         
-        while( i < j){
+//         while( i < j){
             
                 
-            if(nums[i] == nums[i+1]){    
-                int temp = nums[j];
-                nums[j] = nums[i];
-                nums[i] = temp;
-                nums[j] = 101;
-                j--;    
-            }
-            if( i == j){
-                break;
-            }
-            if(nums[i] == nums[j]){
-                nums[j] = 101;
-                j--;
-            }
-            else{ 
-                i++;   
+//             if(nums[i] == nums[i+1]){    
+//                 int temp = nums[j];
+//                 nums[j] = nums[i];
+//                 nums[i] = temp;
+//                 nums[j] = 101;
+//                 j--;    
+//             }
+//             if( i == j){
+//                 break;
+//             }
+//             if(nums[i] == nums[j]){
+//                 nums[j] = 101;
+//                 j--;
+//             }
+//             else{ 
+//                 i++;   
+//             }
+//         }
+//         sort(nums.begin() , nums.end());
+//         i = 0;
+//         while( i < nums.size() && nums[i] <= 100 ){
+//             i++;
+//         }
+//         return i;
+//     }
+    
+        
+//         better version of code 
+       int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if (n <= 1)
+            return n;
+
+        int idx = 1; // Index to track the position to place the next unique element
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] != nums[i-1]) {
+                nums[idx] = nums[i]; // Place the next unique element at the current index
+                idx++; // Increment the index to track the position for the next unique element
             }
         }
-        sort(nums.begin() , nums.end());
-        i = 0;
-        while( i < nums.size() && nums[i] <= 100 ){
-            i++;
-        }
-        return i;
+
+        return idx; // Return the length of the array with duplicates removed
     }
 };
