118177ec7d0f79750d4666bc7755938bf858e7c7
Prabj0t <117665616+Prabj0t@users.noreply.github.com>
2023-07-15 03:08:02 +0530

    Time: 11 ms (15.55%), Space: 13.7 MB (16.95%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -12,40 +12,45 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        vector<vector<int>> ans;
-        
-        map<int , map<int , multiset<int> > > mp;
-        
-        queue<pair<TreeNode*, pair<int , int> > > q; 
-        
-        q.push(make_pair(root , make_pair(0 , 0)));
-        
-        while(!q.empty()){
-            
-            pair<TreeNode*, pair<int , int> > temp = q.front();
-            q.pop();
-            int lvl = temp.second.first;
-            int hd = temp.second.second;
-            
-            mp[hd][lvl].insert(temp.first->val);
-            if(temp.first->left){
-                q.push(make_pair(temp.first->left , make_pair(lvl+1 , hd-1 )));
-            }
-            
-            if(temp.first->right){
-                q.push(make_pair(temp.first->right , make_pair(lvl+1 , hd+1 )));
-            }
-            
+    vector<vector<int>> ans;
+
+    // Using a map to store the nodes according to their horizontal distance and level
+    // map<Horizontal Distance, map<Level, Multiset of Nodes>>
+    map<int, map<int, multiset<int>>> mp;
+
+    queue<pair<TreeNode*, pair<int, int>>> q;
+
+    q.push(make_pair(root, make_pair(0, 0)));
+
+    while (!q.empty()) {
+        pair<TreeNode*, pair<int, int>> temp = q.front();
+        q.pop();
+
+        int lvl = temp.second.first;
+        int hd = temp.second.second;
+
+        // Insert the node's value into the multiset corresponding to its horizontal distance and level
+        mp[hd][lvl].insert(temp.first->val);
+
+        if (temp.first->left) {
+            q.push(make_pair(temp.first->left, make_pair(lvl + 1, hd - 1)));
         }
-        
-        for(auto i : mp){
-            vector<int> levelNodes;
-            for (auto j : i.second) {
-                levelNodes.insert(levelNodes.end(), j.second.begin(), j.second.end());
-                // sort(levelNodes.begin(), levelNodes.end());
-            }
-        ans.push_back(levelNodes);
+
+        if (temp.first->right) {
+            q.push(make_pair(temp.first->right, make_pair(lvl + 1, hd + 1)));
         }
-        return ans;
     }
+
+    for (auto i : mp) {
+        vector<int> levelNodes;
+        for (auto j : i.second) {
+            // Insert the sorted values from the multiset into the levelNodes vector
+            levelNodes.insert(levelNodes.end(), j.second.begin(), j.second.end());
+        }
+        ans.push_back(levelNodes);
+    }
+
+    return ans;
+}
+
 };
