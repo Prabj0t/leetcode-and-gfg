ba8e7f9ccb071a2cb3d499eb370641f2fa3e6b63
Prabj0t <117665616+Prabj0t@users.noreply.github.com>
2023-07-16 01:14:56 +0530

    Time: 4 ms (71.97%), Space: 13.3 MB (58.50%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.cpp
@@ -12,25 +12,29 @@
 class Solution {
 public:
     
-    int sum(TreeNode* root , int &ans ){
-        
-        if(root == NULL){
-            return ans;
+        int sum(TreeNode* root, int& ans) {
+        // If the root is NULL, return the current value of 'ans'
+            if (root == NULL) {
+                return ans;
+            }
+
+        // Check if the left child exists and is a leaf node
+            if (root->left && root->left->left == NULL && root->left->right == NULL) {
+                ans += root->left->val; // Add the value of the left leaf node to 'ans'
+            }
+
+        // Recursively calculate the sum of left leaves in the left and right subtrees
+            int leftSum = sum(root->left, ans);
+            int rightSum = sum(root->right, ans);
+
+            return leftSum + rightSum; // Return the sum of left leaves
         }
-        if( root ->left && root->left->left == NULL && root->left->right == NULL){
-            ans+=root->left->val;
+
+        int sumOfLeftLeaves(TreeNode* root) {
+            int ans = 0;
+            sum(root, ans); // Call the helper function to calculate the sum of left leaves
+
+            return ans; // Return the final sum
         }
-        
-        int lefti = sum(root->left, ans);
-        int righti = sum(root->right, ans);
-        
-        return lefti+righti;
-        
-    }
-    int sumOfLeftLeaves(TreeNode* root) {
-        int ans = 0;
-        sum(root , ans);
-        
-        return ans;
-    }
+
 };
