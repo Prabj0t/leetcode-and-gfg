5414003f231c5614a3ab0ec262c3e15f9d061e88
Prabj0t <117665616+Prabj0t@users.noreply.github.com>
2023-07-18 23:36:59 +0530

    Time: 20 ms (45.38%), Space: 14.2 MB (44.70%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -11,44 +11,71 @@ class Solution {
 public:
     
     // Function to find the lowest common ancestor of nodes p and q in a binary tree
-    TreeNode* solve(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (root == NULL) {
-            return NULL;
-        }
+//     TreeNode* solve(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         if (root == NULL) {
+//             return NULL;
+//         }
 
-        // If the current node is either p or q
-        if (root->val == p->val || root->val == q->val) {
-            return root;
-        }
+//         // If the current node is either p or q
+//         if (root->val == p->val || root->val == q->val) {
+//             return root;
+//         }
 
-        // Recursively search for p and q in the left and right subtrees
-        TreeNode* left = solve(root->left, p, q);
-        TreeNode* right = solve(root->right, p, q);
+//         // Recursively search for p and q in the left and right subtrees
+//         TreeNode* left = solve(root->left, p, q);
+//         TreeNode* right = solve(root->right, p, q);
 
-        // If both p and q are found in different subtrees, the current node is the lowest common ancestor
-        if (left != NULL && right != NULL) {
-            return root;
-        }
+//         // If both p and q are found in different subtrees, the current node is the lowest common ancestor
+//         if (left != NULL && right != NULL) {
+//             return root;
+//         }
         
-        else if (left == NULL && right != NULL) {
-            return right;
-        }
-        else if (left != NULL && right == NULL) {
-            return left;
-        }
+//         else if (left == NULL && right != NULL) {
+//             return right;
+//         }
+//         else if (left != NULL && right == NULL) {
+//             return left;
+//         }
 
-        // If neither p nor q is found in the current subtree, return NULL
-        return NULL;
-    }
+//         // If neither p nor q is found in the current subtree, return NULL
+//         return NULL;
+//     }
 
     
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (root == NULL) {
-            return NULL;
-        }
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         if (root == NULL) {
+//             return NULL;
+//         }
 
-        // Call the 'solve' function to find the lowest common ancestor
-        return solve(root, p, q);
+//         // Call the 'solve' function to find the lowest common ancestor
+//         return solve(root, p, q);
+//     }
+    
+    
+    // Function to find the lowest common ancestor of nodes p and q in a binary tree
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    // Base case: If root is NULL or it matches either p or q, it is the lowest common ancestor
+    if (root == NULL || root == p || root == q) {
+        return root;
     }
+    
+    // Recursively search for p and q in the left and right subtrees
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+    
+    // If both p and q are found in different subtrees, the current node is the lowest common ancestor
+    if (left != NULL && right != NULL) {
+        return root;
+    }
+    // If only p is found, return p as the lowest common ancestor
+    else if (left != NULL) {
+        return left;
+    }
+    // If only q is found, return q as the lowest common ancestor
+    else {
+        return right;
+    }
+}
+
 
 };
