dfd9b5b8a30cb9d36c6cb9110e42d32d32026e96
Prabj0t <117665616+Prabj0t@users.noreply.github.com>
2023-07-24 01:58:56 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (6.53%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -10,20 +10,20 @@
  */
 class Solution {
 public:
-    ListNode* rev(ListNode* &head){
-        if(head == NULL || head->next == NULL){
+    // Helper function to reverse a linked list in place.
+    ListNode* rev(ListNode* &head) {
+        if (head == NULL || head->next == NULL) {
             return head;
         }
     
-        
-        ListNode* chotahead = rev( head->next);
-        head->next -> next = head;
+        ListNode* chotahead = rev(head->next);
+        head->next->next = head;
         head->next = NULL;
         
         return chotahead;
     }
+
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        
         if (head == nullptr || head->next == nullptr || left == right) {
             return head;
         }
@@ -31,46 +31,49 @@ public:
         ListNode* temp = head;
         ListNode* prev = NULL;
         
-        
-        for(int i = 0 ; i < left-1 ; i++){
+        // Move 'temp' and 'prev' pointers to the nodes at positions left-1 and left respectively.
+        for (int i = 0; i < left - 1; i++) {
             prev = temp;
             temp = temp->next; 
         }
         
         if (temp == nullptr) {
-            // 'left' position is beyond the end of the list
+            // 'left' position is beyond the end of the list.
             return head;
         }
         
         ListNode* leftNode = temp;
         ListNode* rightNode = temp; 
         
-        // Move temp to the node at position right+1
+        // Move 'temp' pointer to the node at position right.
         for (int i = 0; i < right - left; i++) {
-           temp = temp->next;
+            temp = temp->next;
         }
         
         if (temp != nullptr) {
+            // Store the next node after the reversed sublist to be connected later.
             rightNode = temp->next;
             temp->next = nullptr;
         }
 
-        // Reverse the sublist from leftNode to rightNode
+        // Reverse the sublist from 'leftNode' to 'rightNode'.
         leftNode = rev(leftNode);
 
-        // Connect the reversed sublist back to the original list
+        // Connect the reversed sublist back to the original list.
         if (prev != nullptr) {
             prev->next = leftNode;
-        } 
-        else {
+        } else {
             head = leftNode;
         }
 
+        // Move 'leftNode' to the end of the reversed sublist.
         while (leftNode->next != nullptr) {
             leftNode = leftNode->next;
         }
         
+        // Connect the end of the reversed sublist to the node at position right+1.
         leftNode->next = rightNode;
+        
         return head;
     }
 };
