7b566d5eb4549f0d688a46bc08b2ddd94a0bfe33
Prabj0t <117665616+Prabj0t@users.noreply.github.com>
2023-07-26 01:51:55 +0530

    Time: 4 ms (33.98%), Space: 6.7 MB (74.07%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,44 +1,46 @@
 class MyStack {
 public:
-    queue<int> q;
+    queue<int> q; // A queue to store elements
+
     MyStack() {
+        // Constructor: Initializing the stack (queue).
     }
     
     void push(int x) {
-        q.push(x);
+        q.push(x); // Add element 'x' to the queue.
     }
     
     int pop() {
         if(q.empty()){
-            return -1;
+            return -1; // If the stack (queue) is empty, return -1 indicating failure.
         }
         for(int i = 0 ; i < q.size()-1 ; i++){
-            int a = q.front();
-            q.pop();
-            q.push(a);
+            int a = q.front(); // Store the front element in a variable 'a'.
+            q.pop(); // Remove the front element from the queue.
+            q.push(a); // Push the front element to the back of the queue (rotate elements).
         }
-        int front = q.front();
-        q.pop();
-        return front;  
+        int front = q.front(); // The last element in the queue is now at the front.
+        q.pop(); // Remove the front element, effectively popping it from the stack.
+        return front; // Return the popped element.
     }
     
     int top() {
-         if(q.empty()){
-            return -1;
+        if(q.empty()){
+            return -1; // If the stack (queue) is empty, return -1 indicating failure.
         }
         for(int i = 0 ; i < q.size()-1 ; i++){
-            int a = q.front();
-            q.pop();
-            q.push(a);
+            int a = q.front(); // Store the front element in a variable 'a'.
+            q.pop(); // Remove the front element from the queue.
+            q.push(a); // Push the front element to the back of the queue (rotate elements).
         }
-        int topi = q.front();
-        q.pop();
-        q.push(topi);
-        return topi; 
+        int topi = q.front(); // The last element in the queue is now at the front.
+        q.pop(); // Remove the front element, but keep it to push back later (to maintain the stack order).
+        q.push(topi); // Push the front element to the back of the queue (restore original order).
+        return topi; // Return the top element without popping it.
     }
     
     bool empty() {
-        return q.empty();
+        return q.empty(); // Check if the stack (queue) is empty.
     }
 };
 
