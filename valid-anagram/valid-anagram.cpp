3bf9ec30752b43fc00cba5846dd8fb1f113730c9
Prabhjot Singh <117665616+Prabj0t@users.noreply.github.com>
2024-12-12 00:11:07 +0530

    Time: 4 ms, Memory: 9.6 MB - LeetHub

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        
-        if( t.size() != s.size()){
+        // If the sizes of s and t are not equal, they can't be anagrams
+        if (s.size() != t.size()) {
             return false;
         }
-        else{
-            map<char , int> mapping;
-            for (int i = 0 ; i < s.size() ; i++){
-                mapping[s[i]]++;
-            }
 
-            for (int j = 0 ; j < t.size() ; j++){
-                if(mapping[t[j]] == 0){
-                    return false;
-                }
-                else{
-                    mapping[t[j]]--;
-                }
+        // Array to count character frequencies, assuming lowercase English letters
+        int count[26] = {0};
+
+        // Iterate over both strings simultaneously
+        for (int i = 0; i < s.size(); i++) {
+            count[s[i] - 'a']++;  // Increment the count for character in s
+            count[t[i] - 'a']--;  // Decrement the count for character in t
+        }
+
+        // If the strings are anagrams, all counts should be 0
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                return false;  // If any count is non-zero, they are not anagrams
             }
         }
+
         return true;
     }
-    
 };
